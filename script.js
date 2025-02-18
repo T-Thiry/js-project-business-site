b9d913c3c57aa8a2733ef482db2bcc58900b3f5c
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-02-18 13:16:03 +0100

    basic respinsivness

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("Script is connected")
+
+
+
+const userName = prompt("What is your name?")
+
+console.log(userName)
+
+
+// We can lso write:
+
+const userPassword = prompt("What is your last password?")
+
+console.log("userPassword", userPassword)
+
+//String
+const myString = "I love coding"
+console.log(typeof myString)
+
+//Number
+console.log(typeof 100)
+console.log(typeof 100.5)
+
+//bigInt
+console.log(typeof 90099021n)
+
+//boolean
+console.log(typeof true)
+console.log(typeof false)
+
+//Undefined
+console.log(typeof undefined)
+
+//Null
+console.log(typeof null)
+
+const firstName = "Mathilda"
+const lastName = "Brunemalm"
+const role = "Teacher"
+let age = 34
+const birthYear = 1990
+
+const fullName = `${firstName} ${lastName}`
+
+console.log(firstName, lastName)
+console.log(fullName)
+
