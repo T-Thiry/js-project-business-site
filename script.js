4d85fa082b983677583b3ca3462edf45ec99c9e0
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-02-22 15:35:07 +0100

    basic responsivness

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerMenuIcon = document.querySelector('.hamburger-menu-icon');
 
 const offScreenMenu = document.querySelector('.off-screen-menu');
 
-hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('active');
+
+hamburgerMenuIcon.addEventListener('click', () => {
+  hamburgerMenuIcon.classList.toggle('active');
   offScreenMenu.classList.toggle('active');
-})
+
+
+  // Toggle scroll prevention
+  if (offScreenMenu.classList.contains('active')) {
+    document.body.style.overflow = 'hidden'; // Disable scroll
+  } else {
+    document.body.style.overflow = ''; // Enable scroll
+  }
+});
